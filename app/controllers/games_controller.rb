abe2624cabc316a6ec522c104f664907bed1fe42
Federico Valenza <federico.valenza5@gmail.com>
2021-01-05 21:05:30 +0100

    new game page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,10 @@
 class GamesController < ApplicationController
   def new
+    alphabet = ('A'..'Z').to_a
+    @letters = []
+    10.times do 
+      @letters << alphabet.sample 
+    end
   end
 
   def score
